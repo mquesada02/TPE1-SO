466661c6ef3a38cf2651aa831186d74bd55213ba
mquesada02 <mquesada@itba.edu.ar>
2023-09-06 14:12:12 -0300

    Modified container_exec.sh

--- container_exec.sh
+++ container_exec.sh
@@ -1,1 +1,7 @@
-docker run -v "${PWD}:/root" --privileged -ti --name SO agodio/itba-so:2.0
+#!/bin/bash
+# docker run -v "${PWD}:/root" --privileged -ti --name SO agodio/itba-so:2.0
+
+sudo docker start SO
+sudo docker exec -ti SO make clean -C/root/
+sudo docker exec -ti SO make -C/root/
+sudo docker stop SO
