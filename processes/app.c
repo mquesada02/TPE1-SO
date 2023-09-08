f7bd7fdd49025e7e3d26142811ed154d0a021213
mquesada02 <mquesada@itba.edu.ar>
2023-09-08 13:53:20 -0300

    Create pipes and create slaves modules

--- processes/app.c
+++ processes/app.c
@@ -4,54 +4,47 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <sys/select.h>
 #include <sys/mman.h>
 #include <fcntl.h>
 
-#define SHMSIZE 1024
+#define ERROR -1
+
+#define SLAVE 0
+
+typedef enum {READ, WRITE} mode;
+
+#define SHM_NAME "/app_view_shm"
+#define SHM_SIZE 1024
 #define rwxrwxrwx 0777
-#define SHMNAME "/app_view_shm"
+
+
 
 char* createSHM(char* shm_name, int size);
 
+void createPipes(int numberOfSlaves, int *slaveREADPipeFDs[], int *slaveWRITEPipeFDs[]);
+
+void createSlave(int slaveID, int *slaveREADPipeFDs[], int *slaveWRITEPipeFDs[], pid_t * slavesPIDs);
+
 int main(int argc, char* argv[]) {
 
-    char* buffer = createSHM(SHMNAME, SHMSIZE);
-    printf("%s", SHMNAME); //lo envio a la salida estandr -> view lo recibe por pipe, o por argumento
+    char* buffer = createSHM(SHM_NAME, SHM_SIZE);
+    printf("%s", SHM_NAME); //lo envio a la salida estandr -> view lo recibe por pipe, o por argumento
+                           // select?
     
-    pid_t pid;
-    int pipefd[2];
+    int filesToProcess = argc-1;                    // cantidad de archivos
+    int numberOfSlaves = filesToProcess/4+1;        // número elegido arbitrariamente
 
-    if (pipe(pipefd)==-1) {
-        perror("pipe");
-        exit(-1);
-    }
+    pid_t slavesPIDs[numberOfSlaves];               // PIDs de los esclavos a generar
+
+    int slaveREADPipeFDs[2][numberOfSlaves];        // pipe que conecta al STDIN del slave
+    int slaveWRITEPipeFDs[2][numberOfSlaves];       // pipe que conecta al STDOUT del slave
+
     
-    // falta pipe para pasarle los archivos, no pasarle argv directo
-    // # de archivos: argc-1
+    createPipes(numberOfSlaves,slaveREADPipeFDs,slaveWRITEPipeFDs);             // crea y cierra los pipes necesarios
+    createSlaves(numberOfSlaves,slaveREADPipeFDs,slaveWRITEPipeFDs,slavesPIDs); // crea y conecta los slaves necesarios
 
-    pid = fork();
-    if (pid == -1) {
-        perror("fork");
-        exit(1);
-    } else if (pid == 0) {
-        /* Child process - c process */
-        close(pipefd[0]); // close READ fd of pipe for CHILD
-        close(1);         // close STDOUT for CHILD
-        dup(pipefd[1]);   // copy WRITE fd of pipe at FD = 1 of CHILD (STDOUT)
-        close(pipefd[1]); // close duplicate fd of pipe
-        execve("./slave",argv, NULL);
-        exit(1);
-    } else {
-        /* Parent process - p process */
-        close(pipefd[1]); // close WRITE fd of pipe for PARENT
-        close(0);         // close STDIN for PARENT
-        dup(pipefd[0]);   // copy READ fd of pipe at FD = 0 of PARENT (STDIN) 
-        close(pipefd[0]); // close duplicate fd of pipe
-        wait(-1);         // wait for child processes to finish
-        char buff[120];
-        read(0,buff,120);
-        printf("%s",buff);
-    }
+    // select for slaveWRITEPipeFDs and maybe slaveREADPipeFDs (consumed?)
 
 
     return 0;
@@ -59,6 +52,51 @@ int main(int argc, char* argv[]) {
 
 char* createSHM(char* shm_name, int size){
     int shm_fd = shm_open("sharedMem", O_CREAT | O_RDWR, rwxrwxrwx);
-    ftruncate(shm_fd, SHMSIZE);
-    return mmap(NULL, SHMSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    ftruncate(shm_fd, size);
+    return mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+}
+/*  Crea 2 pipes para el READ y WRITE amount veces. Cierra ambos FDs en los dos casos. */
+void createPipes(int amount, int *READ[], int *WRITE[]) {
+    for(int i=0; i<amount;i++) {
+        if (pipe(READ[i])==ERROR || pipe(WRITE[i])==ERROR) {
+            perror("Pipe error.");
+            exit(ERROR);
+        }
+        close(READ[i][0]);
+        close(READ[i][1]);
+        close(WRITE[i][0]);
+        close(WRITE[i][1]); 
+    }
+    return;
+}
+
+/* Conecta el pipe en el modo MODE (READ / WRITE) en el fd myfd del proceso */
+void linkPipe(int myfd, int pipeid, int *pipes[], mode MODE) {
+    dup2(myfd,pipes[MODE][pipeid]);
+    return;
+}
+
+void createSlaves(int numberOfSlaves, int *slaveREADPipeFDs[], int *slaveWRITEPipeFDs[], pid_t * slavesPIDs) {
+    for(int slaveID=0;slaveID<numberOfSlaves;slaveID++) {
+        createSlave(slaveID,slaveREADPipeFDs,slaveWRITEPipeFDs,slavesPIDs);
+    }
+    return;
+}
+
+void createSlave(int slaveID, int *slaveREADPipeFDs[], int *slaveWRITEPipeFDs[], pid_t * slavesPIDs) {
+    pid_t forkPID = fork();
+    switch(forkPID) {
+        case ERROR:
+            perror("Fork error.");
+            exit(ERROR);
+        case SLAVE:
+            /* Slave's process */
+            linkPipe(STDOUT_FILENO, slaveID, slaveWRITEPipeFDs, WRITE);
+            linkPipe(STDIN_FILENO, slaveID, slaveREADPipeFDs,READ);
+            break;
+        default:
+            /* Parent's process */
+            slavesPIDs[slaveID] = forkPID;
+    }
+    return;
 }
